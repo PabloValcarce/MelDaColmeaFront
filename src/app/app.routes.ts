dee0fb2d464631e54ae67bb5227a9577c8dd4b68
PabloValcarce <pvalca99@gmail.com>
2024-11-01 15:40:41 +0100

    project creation

---
+++ src/app/app.routes.ts
@@ -0,0 +1,6 @@
+import { Routes } from '@angular/router';
+import { ExampleComponent } from '../shared/components/example/example.component';
+
+export const routes: Routes = [
+    {path:'',component:ExampleComponent}
+];
---
+++ src/shared/services/api.service.ts
@@ -0,0 +1,18 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiService {
+
+  private apiUrl = 'http://localhost:8080/';
+
+  constructor(private http:HttpClient) { }
+
+  getMessage():Observable<string>{
+    return this.http.get<string>(`${this.apiUrl}/roles`);
+  }
+
+}
