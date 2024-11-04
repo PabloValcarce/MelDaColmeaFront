40d932fbd0ad9209ee928db8f570a47decc9b42b
PabloValcarce <pvalca99@gmail.com>
2024-11-04 19:53:58 +0100

    merge

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { HomePageComponent } from '../shared/components/home-page/home-page.component';
+import { ContactComponent } from '../shared/components/contact/contact.component';
 import { LoginComponent } from '../shared/components/login/login.component';
 
 
 export const routes: Routes = [
     {path:'',component:HomePageComponent},
+    {path:'contact',component:ContactComponent},
     {path:'login', component:LoginComponent}
 ];
--- src/shared/components/home-page/home-page.component.ts
+++ src/shared/components/home-page/home-page.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { NavComponent } from '../nav/nav.component';
 import { CommonModule } from '@angular/common';
 import { LoginComponent } from '../login/login.component';
+import { FooterComponent } from '../footer/footer.component';
+import { ContactComponent } from '../contact/contact.component';
 
 @Component({
   selector: 'app-home-page',
@@ -9,7 +11,9 @@ import { LoginComponent } from '../login/login.component';
   imports: [
     CommonModule,
     NavComponent,
-    LoginComponent
+    LoginComponent,
+    FooterComponent,
+    ContactComponent
   ],
   templateUrl: './home-page.component.html',
   styleUrl: './home-page.component.css'
