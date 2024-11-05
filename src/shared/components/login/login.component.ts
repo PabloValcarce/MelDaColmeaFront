1b4faa439773c6efc06dd8c8ef614f6e6bd97358
PabloValcarce <pvalca99@gmail.com>
2024-11-05 13:18:58 +0100

    login fixed siuuu

--- src/shared/components/login/login.component.ts
+++ src/shared/components/login/login.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule, isPlatformBrowser } from '@angular/common';
-import { Component, HostListener, Inject,PLATFORM_ID } from '@angular/core';
+import { Component, Inject, PLATFORM_ID, Renderer2, AfterViewInit, ViewChild, ElementRef, HostListener } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { NavComponent } from '../nav/nav.component';
 
@@ -12,23 +12,68 @@ import { NavComponent } from '../nav/nav.component';
     NavComponent
   ],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  styleUrls: ['./login.component.css']
 })
-export class LoginComponent {
+export class LoginComponent implements AfterViewInit {
   isLoginView: boolean = true;
   isMobile: boolean = false;
+  showLoginBox:boolean = true;
+  showRegisterBox:boolean = false;
 
-  constructor(@Inject(PLATFORM_ID) private platformId: Object) {
+  // Definir las vistas de los formularios y contenedores con ViewChild
+  @ViewChild('formularioLogin') formulario_login!: ElementRef;
+  @ViewChild('formularioRegister') formulario_register!: ElementRef;
+  @ViewChild('contenedorLoginRegister') contenedor_login_register!: ElementRef;
+  @ViewChild('cajaTraseraLogin') caja_trasera_login!: ElementRef;
+  @ViewChild('cajaTraseraRegister') caja_trasera_register!: ElementRef;
+
+  constructor(
+    @Inject(PLATFORM_ID) private platformId: Object,
+    private renderer: Renderer2
+  ) {}
+
+  ngOnInit() {
+    // Establecer estilos de fondo en el body
     if (isPlatformBrowser(this.platformId)) {
       this.isMobile = window.innerWidth <= 850;
-      
+      this.renderer.setStyle(document.body, 'background-color', '#121212');
+      this.renderer.setStyle(document.body, 'height', '70%');
+    }
+    this.updateLayout();
+  }
+  @HostListener('window:resize', ['$event'])
+  onResize(event: any) {
+    this.updateLayout();
+  }
+  // Actualiza el estado basado en el tamaño de la ventana
+  updateLayout() {
+    this.isMobile = window.innerWidth <= 850;
+    if (!this.isMobile) {
+      // Si estamos en una pantalla grande, mostramos ambas cajas
+      this.showLoginBox = true;
+      this.showRegisterBox = true;
+    } else {
+      // Si estamos en una pantalla pequeña, mostramos solo la caja de login por defecto
+      this.showLoginBox = true;
+      this.showRegisterBox = false;
     }
   }
-  changeBackground() {
-    document.body.classList.add('new-background');
+  ngAfterViewInit() {
+    // Asegurarse de que los elementos se hayan inicializado
+    if (isPlatformBrowser(this.platformId)) {
+      // No necesitamos querySelector, ya que @ViewChild ya nos da acceso a los elementos
+    }
   }
+
   toggleView(view: string) {
     this.isLoginView = (view === 'login');
+    if (view === 'login') {
+      this.showLoginBox = true;
+      this.showRegisterBox = false;
+    } else if (view === 'register') {
+      this.showLoginBox = false;
+      this.showRegisterBox = true;
+    }
   }
 
   onLogin() {
@@ -39,5 +84,52 @@ export class LoginComponent {
   onRegister() {
     // Maneja el registro
     console.log('Registrarse');
-  }  
+  }
+
+  // anchoPage() {
+  //   if (window.innerWidth > 850) {
+  //     this.renderer.setStyle(this.caja_trasera_register.nativeElement, 'display', 'block');
+  //     this.renderer.setStyle(this.caja_trasera_login.nativeElement, 'display', 'block');
+  //   } else {
+  //     this.renderer.setStyle(this.caja_trasera_register.nativeElement, 'display', 'block');
+  //     this.renderer.setStyle(this.caja_trasera_register.nativeElement, 'opacity', '1');
+  //     this.renderer.setStyle(this.caja_trasera_login.nativeElement, 'display', 'none');
+  //     this.renderer.setStyle(this.formulario_login.nativeElement, 'display', 'block');
+  //     this.renderer.setStyle(this.contenedor_login_register.nativeElement, 'left', '0px');
+  //     this.renderer.setStyle(this.formulario_register.nativeElement, 'display', 'none');
+  //   }
+  // }
+
+  // register() {
+  //   if (window.innerWidth > 850) {
+  //     this.renderer.setStyle(this.formulario_register.nativeElement, 'display', 'block');
+  //     this.renderer.setStyle(this.contenedor_login_register.nativeElement, 'left', '410px');
+  //     this.renderer.setStyle(this.formulario_login.nativeElement, 'display', 'none');
+  //     this.renderer.setStyle(this.caja_trasera_register.nativeElement, 'opacity', '0');
+  //     this.renderer.setStyle(this.caja_trasera_login.nativeElement, 'opacity', '1');
+  //   } else {
+  //     this.renderer.setStyle(this.formulario_register.nativeElement, 'display', 'block');
+  //     this.renderer.setStyle(this.contenedor_login_register.nativeElement, 'left', '0px');
+  //     this.renderer.setStyle(this.formulario_login.nativeElement, 'display', 'none');
+  //     this.renderer.setStyle(this.caja_trasera_register.nativeElement, 'display', 'none');
+  //     this.renderer.setStyle(this.caja_trasera_login.nativeElement, 'display', 'block');
+  //     this.renderer.setStyle(this.caja_trasera_login.nativeElement, 'opacity', '1');
+  //   }
+  // }
+
+  // iniciarSesion() {
+  //   if (window.innerWidth > 850) {
+  //     this.renderer.setStyle(this.formulario_login.nativeElement, 'display', 'block');
+  //     this.renderer.setStyle(this.contenedor_login_register.nativeElement, 'left', '10px');
+  //     this.renderer.setStyle(this.formulario_register.nativeElement, 'display', 'none');
+  //     this.renderer.setStyle(this.caja_trasera_register.nativeElement, 'opacity', '1');
+  //     this.renderer.setStyle(this.caja_trasera_login.nativeElement, 'opacity', '0');
+  //   } else {
+  //     this.renderer.setStyle(this.formulario_login.nativeElement, 'display', 'block');
+  //     this.renderer.setStyle(this.contenedor_login_register.nativeElement, 'left', '0px');
+  //     this.renderer.setStyle(this.formulario_register.nativeElement, 'display', 'none');
+  //     this.renderer.setStyle(this.caja_trasera_register.nativeElement, 'display', 'block');
+  //     this.renderer.setStyle(this.caja_trasera_login.nativeElement, 'display', 'none');
+  //   }
+  // }
 }
