80b91434ca9990fa09f55d1bbfb3665ca15f9cb0
PabloValcarce <pvalca99@gmail.com>
2024-11-07 10:24:16 +0100

    creacion de menu de contacto y variables

--- src/shared/components/contact/contact.component.ts
+++ src/shared/components/contact/contact.component.ts
@@ -1,5 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { NavComponent } from '../nav/nav.component';
 
 @Component({
@@ -7,11 +8,31 @@ import { NavComponent } from '../nav/nav.component';
   standalone: true,
   imports: [
     CommonModule,
-    NavComponent
+    NavComponent,
+    FormsModule
+    
   ],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.css'
 })
 export class ContactComponent {
+  contactForm = {
+    name: '',
+    email: '',
+    subject: '',
+    message: ''
+  };
 
+  // Método que se ejecuta cuando el formulario se envía
+  onSubmit(): void {
+    if (this.contactForm.name && this.contactForm.email && this.contactForm.subject && this.contactForm.message) {
+      console.log('Formulario enviado:', this.contactForm);
+      // Aquí puedes enviar el formulario a un servidor o hacer lo que sea necesario.
+      alert('¡Mensaje enviado exitosamente!');
+      // Limpiar el formulario
+      this.contactForm = { name: '', email: '', subject: '', message: '' };
+    } else {
+      alert('Por favor, completa todos los campos.');
+    }
+  }
 }
