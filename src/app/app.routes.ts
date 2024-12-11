bfdc09e0d77ca18b045a6f821bde5b775046e142
PabloValcarce <pvalca99@gmail.com>
2024-12-11 13:33:37 +0100

    cart form

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,11 +3,16 @@ import { HomePageComponent } from '../shared/components/home-page/home-page.comp
 import { ContactComponent } from '../shared/components/contact/contact.component';
 import { LoginComponent } from '../shared/components/login/login.component';
 import { CartComponent } from '../shared/components/cart/cart.component';
-
+import { CartFormComponent } from '../shared/components/cart/cart-form/cart-form.component';
 
 export const routes: Routes = [
-    {path:'',component:HomePageComponent},
-    {path:'contact',component:ContactComponent},
-    {path:'login', component:LoginComponent},
-    {path:'cart',component:CartComponent}
+    { path: '', component: HomePageComponent },
+    { path: 'contact', component: ContactComponent },
+    { path: 'login', component: LoginComponent },
+    { 
+        path: 'cart', component: CartComponent, 
+        children: [
+            { path: 'form', component: CartFormComponent }
+        ]
+    },
 ];
